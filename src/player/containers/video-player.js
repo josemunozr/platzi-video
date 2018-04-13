6ab9451c8f19263ce6a81c94cbb639790528c634
José Muñoz R <jozeepp@gmail.com>
2018-04-12 22:40:16 -0300

    Creación UI Play/Pause

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
 import Title from '../components/title';
+import PlayPause from '../components/play-pause';
 class VideoPlayer extends Component {
+  state = {
+    pause: false
+  }
+  togglePlay = () => {
+    this.setState({
+      pause: !this.state.pause
+    })
+  }
   render() { 
     return ( 
       <VideoPlayerLayout>
         <Title 
           title="Titulo Video!"
+        />
+        <PlayPause
+          pause={this.state.pause}
+          handleClick={this.togglePlay}
         />
         <Video 
           autoplay={true}
