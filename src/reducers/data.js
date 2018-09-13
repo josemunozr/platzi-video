c910e706ae74fa653404c72d038de12bb3bf9f99
José Muñoz R <jozeepp@gmail.com>
2018-09-12 23:58:28 -0300

    se realiza busqueda a traves de actions de reducers

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,7 +1,15 @@
 function data (state, action) {
   switch (action.type) {
     case 'SEARCH_VIDEO':
-      return state;
+    const list = state.data.categories[1].playlist;
+    const result = list.filter(
+        item => item.author
+                  .includes(action.payload.query)
+      )
+      return {
+        ...state,
+        search: result
+      };
     default:
       return state;
   }
