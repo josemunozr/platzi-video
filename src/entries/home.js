035190c44fc82a7e0391bc295a2c170dca11025a
José Muñoz R <jozeepp@gmail.com>
2018-10-03 23:40:38 -0300

    redux-devtools-extension

--- src/entries/home.js
+++ src/entries/home.js
@@ -5,20 +5,22 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducer from '../reducers/index';
 import { Map as map} from 'immutable';
-
-const logger = ({ getState, dispatch }) => next => action => {
-  console.log('viejo estado', getState().toJS());
-  console.log('acción', action );
-  const value = next(action);
-  console.log('nuevo estado', getState().toJS());
-  return value;
-}
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+// const logger = ({ getState, dispatch }) => next => action => {
+//   console.log('viejo estado', getState().toJS());
+//   console.log('acción', action );
+//   const value = next(action);
+//   console.log('nuevo estado', getState().toJS());
+//   return value;
+// }
 
 const store = createStore(
   reducer,
   map(),
-  applyMiddleware(logger)
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(
+    applyMiddleware(logger)
+  )
 );
 
 
