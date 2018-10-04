f6255992e94f087592efeb9a1f71b1080e038267
José Muñoz R <jozeepp@gmail.com>
2018-10-03 22:59:02 -0300

    custom middleware logger

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,14 +2,23 @@ import React from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from '../reducers/index';
 import { Map as map} from 'immutable';
+
+const logger = ({ getState, dispatch }) => next => action => {
+  console.log('viejo estado', getState().toJS());
+  console.log('acción', action );
+  const value = next(action);
+  console.log('nuevo estado', getState().toJS());
+  return value;
+}
 
 const store = createStore(
   reducer,
   map(),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(logger)
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 
