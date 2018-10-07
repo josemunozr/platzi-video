16c630892d2922e694c64af4d89cf08e8c154296
José Muñoz R <jozeepp@gmail.com>
2018-10-06 23:59:25 -0300

    parametrizacion router al abrir y cerrar modal

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import './media.css';
 
 class Media extends PureComponent {
@@ -19,19 +20,30 @@ class Media extends PureComponent {
 	  const { cover, title, author} = this.props;
 
 	  return (
-	    <div className='Media' onClick={this.handleClick}>
-	      <div className='Media-cover'>
-	        <img
-						src={cover}
-						alt=''
-						width={260}
-						height={160}
-	          className='Media-image'
-	        />
-	      </div>
-				<h3 className='Media-title'>{title}</h3>
-				<p className='Media-author'>{author}</p>
-	    </div>
+			<Link 
+				to={{
+					pathname: "/videos",
+					search: `?id=${this.props.id}`,
+					state: {
+						modal: true,
+						id: this.props.id
+					}
+				}}
+			>
+				<div className='Media' onClick={this.handleClick}>
+					<div className='Media-cover'>
+						<img
+							src={cover}
+							alt=''
+							width={260}
+							height={160}
+							className='Media-image'
+						/>
+					</div>
+					<h3 className='Media-title'>{title}</h3>
+					<p className='Media-author'>{author}</p>
+				</div>
+			</Link>
 	  );
 	}
 }
